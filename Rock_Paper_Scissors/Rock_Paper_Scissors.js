b8cab0c813244e5607db9804ab6a352376d19740
khaled <benchikha.khaled@gmail.com>
2024-08-18 18:03:58 +0100

    Add files via upload

---
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.js
@@ -0,0 +1,87 @@
+let s,cm;
+      let score = JSON.parse(localStorage.getItem('score')) || {
+        win: 0,
+        lose: 0,
+        tie: 0
+      };
+      document.querySelector('.score').innerHTML = `Win: ${score.win} | Lose: ${score.lose} | Tie: ${score.tie}`;
+      
+
+      function Cpick(){
+        let c;
+        let r = Math.random();
+        if(r>=0 && r<1/3){
+        c = 'Rock';
+        }else if(r>=1/3 && r<2/3){
+        c = 'Paper';
+        }else if(r>=2/3 && r<1){
+        c = 'Scissors';
+        }
+        return c;
+      }
+
+      function ResultR(cm){
+        let r;
+        if(cm === 'Rock'){
+        r = 'Tie';
+        }else if(cm === 'Paper'){
+        r = 'Lose';
+        }else {
+        r = 'Win';
+        }
+        return r;
+      }
+
+      function ResultP(cm){
+        let r;
+        if(cm === 'Paper'){
+        r = 'Tie';
+        }else if(cm === 'Scissors'){
+        r = 'Lose';
+        }else {
+        r = 'Win';
+        }
+        return r;
+      }
+
+      function ResultS(cm){
+        let r;
+        if(cm === 'Scissors'){
+        r = 'Tie';
+        }else if(cm === 'Rock'){
+        r = 'Lose';
+        }else {
+        r = 'Win';
+        }
+        return r;
+      }
+
+      function PlayGame(player){
+        cm = Cpick();
+        if (player === 'Rock') {
+          s = ResultR(cm);
+        }else if(player === 'Paper'){
+          s = ResultP(cm);
+        }else{
+          s = ResultS(cm);
+        }
+        document.querySelector('.move').innerHTML = `
+          You
+          <img src="RPS_img/${player}-emoji.png">
+          <img src="RPS_img/${cm}-emoji.png">
+          Computer`;
+
+        if (s === 'Win') {
+          score.win++;
+        }else if(s === 'Lose'){
+          score.lose++;
+        }else{
+          score.tie++;
+        } 
+
+        document.querySelector('.result').innerHTML = `${s}.`;
+
+        localStorage.setItem('score',JSON.stringify(score));
+        document.querySelector('.score').innerHTML = `Win: ${score.win} | Lose: ${score.lose} | Tie: ${score.tie}`;
+
+      }
